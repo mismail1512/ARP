3600c8456a83071260bf8adb787ff3c0a0693038
ahmedmsalah99 <45595018+ahmedmsalah99@users.noreply.github.com>
2025-01-05 14:45:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+# Ensure the log folder exists
+LOG_FOLDER="./log"
+if [ ! -d "$LOG_FOLDER" ]; then
+    echo "Log folder not found. Creating it..."
+    mkdir -p "$LOG_FOLDER"
+else
+    echo "Log folder already exists."
+fi
 ./build/obstacles_gen &
 ./build/targets_gen &
 ./build/board &
